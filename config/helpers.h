9a94c69c30a17e4ca10c9e15796a2a4ac4b0ae65
Cédric Prezelin <cedric.prezelin-ext@ixxitechside.com>
2023-06-24 16:27:26 +0200

    feat(default): disable bspc layout toggle on shift press

--- config/helpers.h
+++ config/helpers.h
@@ -6,17 +6,21 @@
 // shift modifier helper
 #include "../zmk-nodefree-config/helper.h"
 
-#define DEF_MORPH(NAME, MODS, CODE, MOD_CODE) \ 
+#define DEF_LSHFT_KP_MORPH(NAME, CODE, MOD_CODE) \ 
      ZMK_BEHAVIOR(NAME, mod_morph, \
                bindings = <&kp CODE>, <&kp MOD_CODE>; \
-               mods = <(MODS)>; \
+               mods = <(MOD_LSFT)>; \
      )
 
-#define DEF_LSHFT_MORPH(NAME, CODE, MOD_CODE) \ 
-    DEF_MORPH(NAME, MOD_LSFT, CODE, MOD_CODE)
+#define DEF_LT_DISABLED_ON_SFT(NAME, LAYER, CODE) \ 
+     ZMK_BEHAVIOR(NAME, mod_morph, \
+               bindings = <&lt LAYER CODE>, <&kp CODE>; \
+               mods = <(MOD_LSFT)>; \
+     )
 
 // Home Row mode optimizer
 #include "../zmk-nodefree-config/keypos_def/keypos_42keys.h"
+
 #define LEFT_KEYS LT0 LT1 LT2 LT3 LT4 LT5 LM0 LM1 LM2 LM3 LM4 LM5 LB0 LB1 LB2 LB3 LB4 LB5
 #define RIGHT_KEYS RT0 RT1 RT2 RT3 RT4 RT5 RM0 RM1 RM2 RM3 RM4 RM5 RB0 RB1 RB2 RB3 RB4 RB5
 #define THUMBS LH2 LH1 LH0 RH0 RH1 RH2
